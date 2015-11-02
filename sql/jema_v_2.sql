ab1cc180d49f3d4027c213a124eba3e5c6b515b8
redvox27 <v.t.luder@st.hanze.nl>
2015-11-02 16:46:29 +0100

    wack

--- sql/jema_v_2.sql
+++ sql/jema_v_2.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 01 nov 2015 om 14:40
+-- Gegenereerd op: 02 nov 2015 om 16:42
 -- Serverversie: 10.0.17-MariaDB
 -- PHP-versie: 5.5.30
 
@@ -87,12 +87,24 @@ CREATE TABLE `order` (
 --
 
 CREATE TABLE `product` (
-  `product code` int(10) NOT NULL,
+  `product_code` int(10) NOT NULL,
   `product_naam` varchar(20) NOT NULL,
   `boolean` int(1) NOT NULL,
-  `beschrijving` varchar(150) NOT NULL,
+  `beschrijving` text NOT NULL,
   `prijs` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `product`
+--
+
+INSERT INTO `product` (`product_code`, `product_naam`, `boolean`, `beschrijving`, `prijs`) VALUES
+(1, 'office_2016', 0, 'Microsoft Word, of meestal alleen Word, is een van de meest gebruikte tekstverwerkers ter wereld. Het is gemaakt door Microsoft.\r\n\r\nMicrosoft Word werkt op Windows en OS X. Met behulp van Wine werken versies pre-2013 ook op Linux. Er zijn verschillende commerciële, gratis en vrije alternatieven beschikbaar, zoals LibreOffice, OpenOffice.org en AbiWord. Tot in het begin van de jaren jaren 90 was WordPerfect de meest gebruikte tekstverwerker, maar toen voor de pc het DOS-besturingssysteem door Windows werd vervangen, werd Word de algemeen gebruikte tekstverwerker. Word is in veel talen beschikbaar.', 420),
+(2, 'Acrobat Standard DC', 1, 'Acrobat DC is de volledig nieuwe ontworpen desktop versie van de beste PDF-oplossing. Het omvat een mobiele app zodat je PDFjes kunt invullen, ondertekenen, en delen op elk apparaat. En met Document Cloud-services kun je vanaf elke locatie PDFjes maken,exporteren,bewerken en volgen, en op al je apparaten verbonden blivjen met je recente bestanden', 5),
+(3, '3DS Max', 1, '3DS Max is een computerprogramma voor het vaardigen van 3D ruimtelijke modellen met behulp van vectoren en coördinaten. De gemaakte oppervlaktes kunnen worden bekleed met textures, de  geconstrueerde objecten kunnen bij elkaar in dezelfde virtuele ruimte worden geplaatst Daarnaast kan een virtuele camera met instelbare virtuele lenzen door deze scène worden gestuurd volgens een aan te geven route.', 50),
+(4, 'lynda.com', 0, 'Our teachers are effective, passionate educators, who are also respected authorities in software, creative, and business fields. They are here to share their expertise in dozens of topics with you, with courses organized into these eight subject areas.', 10),
+(5, 'Signavio', 0, 'Signavio combines the right business process management strategy with the ideal tool. The Signavio Process Editor offers a full range of functions and serves as an operative platform to include all process participants in business process management. Thanks to QuickModel’s user-friendly tabular process representation, even process participants with no experience in the BPM field can actively contribute to the documentation and optimization of processes. Collaborative BPM is the strategy for your success!', 75),
+(6, 'McAfee', 1, 'McAfee, onderdeel van Intel Security, is het grootste bedrijf ter wereld dat zich specifiek toelegt op security-technologie. McAfee levert proactieve en beproefde oplossingen en services om systemen en netwerken over de hele wereld te beveiligen. Het beschermt consumenten en bedrijven van klein tot groot tegen de nieuwste malware en opkomende onlinebedreigingen. Onze oplossingen zijn speciaal ontworpen om met elkaar samen te werken: ze combineren antimalware-, antispyware- en antivirussoftware met functies voor security-beheer. Deze zorgen niet alleen voor een ongeëvenaard realtime inzicht en analyses, maar verlagen ook het risico, zorgen voor compliance, verbeteren de internet-security en helpen bedrijven efficiënter te werken.', 15);
 
 -- --------------------------------------------------------
 
@@ -143,7 +155,7 @@ ALTER TABLE `order`
 -- Indexen voor tabel `product`
 --
 ALTER TABLE `product`
-  ADD PRIMARY KEY (`product code`);
+  ADD PRIMARY KEY (`product_code`);
 
 --
 -- Indexen voor tabel `software deliverer`
@@ -169,7 +181,7 @@ ALTER TABLE `order`
 -- AUTO_INCREMENT voor een tabel `product`
 --
 ALTER TABLE `product`
-  MODIFY `product code` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `product_code` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT voor een tabel `software deliverer`
 --
