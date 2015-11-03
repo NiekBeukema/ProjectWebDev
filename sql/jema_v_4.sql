a4a7f2ab135ff59e2b444dcd4c6adf47c11e0d0b
redvox27 <v.t.luder@st.hanze.nl>
2015-11-03 09:53:40 +0100

    hier niek met je hoofd

--- sql/jema_v_4.sql
+++ sql/jema_v_4.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 03 nov 2015 om 09:23
+-- Gegenereerd op: 03 nov 2015 om 09:51
 -- Serverversie: 10.0.17-MariaDB
 -- PHP-versie: 5.5.30
 
@@ -19,6 +19,66 @@ SET time_zone = "+00:00";
 --
 -- Database: `jema`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `bestaat_uit`
+--
+
+CREATE TABLE `bestaat_uit` (
+  `order_order_id` int(11) NOT NULL,
+  `product_product_code` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `gebruiker`
+--
+
+CREATE TABLE `gebruiker` (
+  `gebruiker id` int(8) NOT NULL,
+  `user_role` varchar(10) NOT NULL,
+  `voornaam` varchar(15) NOT NULL,
+  `achternaam` varchar(15) NOT NULL,
+  `inlognaam` varchar(15) NOT NULL,
+  `wachtwoord` text NOT NULL,
+  `plaats` varchar(15) DEFAULT NULL,
+  `straatnaam` varchar(15) DEFAULT NULL,
+  `postcode` text NOT NULL,
+  `telefoonnummer` int(10) NOT NULL,
+  `e_mail` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `gebruiker`
+--
+
+INSERT INTO `gebruiker` (`gebruiker id`, `user_role`, `voornaam`, `achternaam`, `inlognaam`, `wachtwoord`, `plaats`, `straatnaam`, `postcode`, `telefoonnummer`, `e_mail`) VALUES
+(1, 'customer', 'henk', 'henkinson', 'henk', 'henk', 'henkstad', 'kampakkers', '9531HV', 599236503, 'henkinson@hotmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `levert_een`
+--
+
+CREATE TABLE `levert_een` (
+  `software_deliverer_deliver_id` int(10) NOT NULL,
+  `product_product_code` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `order`
+--
+
+CREATE TABLE `order` (
+  `order_id` int(10) NOT NULL,
+  `order_datum` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -49,9 +109,50 @@ INSERT INTO `product` (`product_code`, `product_naam`, `boolean`, `beschrijving`
 (8, 'Adobe Photoshop Elem', 1, 'Van middelmatige snapshot naar lievelingsfoto. Breng snel bewerkingen aan, voeg als een professional artistieke elementen toe of transformeer je foto s om je vrienden en familie te imponeren. In Photoshop Elements kun je stapsgewijze instructies volgen of kun je het voor je laten doen.Al je herinneringen binnen handbereik. Orden, label, vind en bekijk je foto’s op jouw manier.Toon je creatieve kant. Maak plakboekpagina’s, kaarten en kalenders om blijvende herinneringen te delen.Deel je herinneringen met de mensen die belangrijk voor je zijn, op de manier zoals jij dat wilt – Facebook, YouTube, Vimeo, Twitter en meer.*', 35),
 (9, 'Adobe Flash Builder ', 1, 'Adobe® Flash® Builder® 4.7 software is a development environment for building games and applications using the ActionScript® language and the open source Flex framework. Flash Builder Premium includes professional testing tools such as profilers, network monitoring, and unit testing support.', 20);
 
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `software deliverer`
+--
+
+CREATE TABLE `software deliverer` (
+  `deliverer id` int(10) NOT NULL,
+  `naam` varchar(20) NOT NULL,
+  `plaats` varchar(20) NOT NULL,
+  `adres` text NOT NULL,
+  `postcode` text NOT NULL,
+  `telefoonnummer` int(10) NOT NULL,
+  `e-mail` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 --
 -- Indexen voor geëxporteerde tabellen
 --
+
+--
+-- Indexen voor tabel `bestaat_uit`
+--
+ALTER TABLE `bestaat_uit`
+  ADD PRIMARY KEY (`order_order_id`,`product_product_code`);
+
+--
+-- Indexen voor tabel `gebruiker`
+--
+ALTER TABLE `gebruiker`
+  ADD PRIMARY KEY (`gebruiker id`);
+
+--
+-- Indexen voor tabel `levert_een`
+--
+ALTER TABLE `levert_een`
+  ADD PRIMARY KEY (`software_deliverer_deliver_id`,`product_product_code`),
+  ADD UNIQUE KEY `software_deliverer_deliver_id` (`software_deliverer_deliver_id`);
+
+--
+-- Indexen voor tabel `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`order_id`);
 
 --
 -- Indexen voor tabel `product`
@@ -60,14 +161,35 @@ ALTER TABLE `product`
   ADD PRIMARY KEY (`product_code`);
 
 --
+-- Indexen voor tabel `software deliverer`
+--
+ALTER TABLE `software deliverer`
+  ADD PRIMARY KEY (`deliverer id`);
+
+--
 -- AUTO_INCREMENT voor geëxporteerde tabellen
 --
 
+--
+-- AUTO_INCREMENT voor een tabel `gebruiker`
+--
+ALTER TABLE `gebruiker`
+  MODIFY `gebruiker id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT voor een tabel `order`
+--
+ALTER TABLE `order`
+  MODIFY `order_id` int(10) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT voor een tabel `product`
 --
 ALTER TABLE `product`
   MODIFY `product_code` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT voor een tabel `software deliverer`
+--
+ALTER TABLE `software deliverer`
+  MODIFY `deliverer id` int(10) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
